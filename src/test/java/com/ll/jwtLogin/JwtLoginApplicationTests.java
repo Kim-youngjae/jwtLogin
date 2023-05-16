39839e45794ad8272c5809ac3df8b5c5d153174f
youngjae459 <youngjae459@gmail.com>
2023-05-16 15:07:48 +0900

    chore: 주석 추가

--- src/test/java/com/ll/jwtLogin/JwtLoginApplicationTests.java
+++ src/test/java/com/ll/jwtLogin/JwtLoginApplicationTests.java
@@ -14,7 +14,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 @SpringBootTest
 class JwtLoginApplicationTests {
 	@Value("${custom.jwt.secretKey}")
-	private String secretKeyPlain;
+	private String secretKeyPlain; // 원래 시크릿키 원문
 
 	@Test
 	@DisplayName("secretKey 키가 존재해야한다.")
@@ -26,11 +26,9 @@ class JwtLoginApplicationTests {
 	@DisplayName("sercretKey 원문으로 hmac 암호화 알고리즘에 맞는 SecretKey 객체를 만들 수 있다.")
 	void t2() {
 		// 키를 Base64 인코딩 한다.
-		String keyBase64Encoded = Base64.getEncoder().encodeToString(secretKeyPlain.getBytes());
+		String keyBase64Encoded = Base64.getEncoder().encodeToString(secretKeyPlain.getBytes()); // 원문을 Base64로 인코딩
 		// Base64 인코딩된 키를 이용하여 SecretKey 객체를 만든다.
-		SecretKey secretKey = Keys.hmacShaKeyFor(keyBase64Encoded.getBytes());
-
-		System.out.println(secretKey);
+		SecretKey secretKey = Keys.hmacShaKeyFor(keyBase64Encoded.getBytes()); // 인코딩 한 결과를 암호화
 
 		assertThat(secretKey).isNotNull();
 	}
